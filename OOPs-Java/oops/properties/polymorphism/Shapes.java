bfc0780a736aeaa1bc63d0c124bf99a58c1c61f4
Atul Singh <cocatul11@gmail.com>
2024-07-13 13:20:57 +0530

    refactor: Remove unnecessary polymorphism classes and files

--- OOPs-Java/oops/properties/polymorphism/Shapes.java
+++
@@ -1,9 +0,0 @@
-package oops.properties.polymorphism;
-
-// This is an example of Dynamic Polymorphism
-
-public class Shapes {
-    void area () {
-        System.out.println("I am inside of Shapes Class");
-    }
-}
